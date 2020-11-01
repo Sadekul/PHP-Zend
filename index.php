02ac33cc72f71fecc543de6852a1e248e1d7f2bc
muhaiminulIslamIkhlam <muhaiming2c@gmail.com>
2020-11-02 00:35:11 +0600

    ...function for pass unlimited parameter

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php 
 
-echo "setu";
+$a="setu";
+$b="sadekul";
+$c= &$a;
+echo $c."<br>";
+define("a", "bangladesh", true);
+define("A", "lue", true);
+echo a."<br>";
+
+function myfun(...$val){
+	$val;
+	echo "<pre>";
+	print_r($val);
+	echo "<pre>";
+}
+	myfun(3,4,5,setu,4.5);
+
 
  ?>
